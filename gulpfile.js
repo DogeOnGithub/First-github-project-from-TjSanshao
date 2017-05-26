75b8f19a473108618975a15dbbb89e4ae762bfbf
TjSanshao <1305705188@qq.com>
2017-05-26 13:15:16 +0800

    课件

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// 静态服务器
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp
+        .watch('**/*.html')
+        .on('change', browserSync.reload);
+});
